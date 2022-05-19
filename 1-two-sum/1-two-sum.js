87942f8bc300a4367b546cdd1408c35b5ae56758
Hemant Negi <nhemnt@gmail.com>
2022-05-20 00:01:53 +0530

    Time: 76 ms (83.15%), Space: 42.7 MB (52.51%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const obj = {};
+    // console.log({nums, target})
+    for(let i=0; i<nums.length; i++){
+        const rn = target - nums[i];
+        if(obj[rn] !== undefined){
+            return i < obj[rn] ? [i, obj[rn]] : [obj[rn], i];
+        }
+        obj[nums[i]] = i;
+    }
+    // console.log(obj);
+};
