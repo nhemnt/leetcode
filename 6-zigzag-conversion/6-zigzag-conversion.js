f32137b9b793ca334f5756bda81a89a3a46eb66b
Hemant Negi <nhemnt@gmail.com>
2022-05-29 23:29:19 +0530

    Time: 126 ms (49.60%), Space: 45.5 MB (95.49%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.js
+++ 6-zigzag-conversion/6-zigzag-conversion.js
@@ -4,51 +4,22 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
-    if(numRows < 2) return s; 
-    if(numRows === 2){
-        let lOutput = ''; rOutput = ''
-        for(let i=0; i<s.length; i++){
-            if(i%2 === 0){
-                lOutput +=s[i];
-            }else {
-                rOutput += s[i]
-            }
-        }
-        return lOutput + rOutput;
-    }
-    const result =  Array(numRows).fill([]);
-    for(let i=0; i<result.length; i++){
-        result[i] = Array(s.length).fill(0);
-    }
-  
-    let x = -1; y=0; asc = true;
-    for(let i=0; i<s.length;i++){
-        if(asc){
-            x++;
-        }
-        console.log({x,y,s: s[i]});
-        result[x][y] = s[i];
-        if(x === numRows-1 && asc){
-            asc = false;
-            if(x !== 0) x--;
-            y++
-        }else if(!asc && x == 1){
-            x = -1;
-            y++;
-            asc = true;
-        }else if(!asc){
-            if(x !== 0) x--;
-            y++;
-        }
-    }
+    if(numRows === 1) return s; 
     
-    let output = ''
-    for(let i=0; i<result.length; i++){
-        for(let j=0; j<result[i].length; j++){
-            if(result[i][j]) output += result[i][j];
+    let down = false, rowI = 0;
+    const outputArr = Array(numRows).fill('');
+    for(let i = 0; i<s.length;i++){
+        if(rowI === 0) down = true;
+        if(rowI === numRows-1) down = false;
+        
+        outputArr[rowI] += s[i];
+        if(down){
+            rowI++
+        }else{
+            rowI--
         }
+        
     }
-
-    return output;
+    return outputArr.join('')
 
 }
