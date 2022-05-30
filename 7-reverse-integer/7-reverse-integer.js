e599559cb7e146f60c78f8a17e8644e9a1728ab9
Hemant Negi <nhemnt@gmail.com>
2022-05-31 00:05:15 +0530

    Time: 76 ms (86.17%), Space: 43.9 MB (64.40%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.js
+++ 7-reverse-integer/7-reverse-integer.js
@@ -5,10 +5,8 @@
 var reverse = function(x) {
     const twoPowerThirtyOne = Math.pow(2, 31); 
     const max = twoPowerThirtyOne - 1;
-    const min = -1 * twoPowerThirtyOne;
     let isNegative = x < 0;    
     const reverseSting = parseInt(x.toString().split("").reverse().join(""))
-    const result  = isNegative ? -1 * reverseSting: reverseSting;
 
-    return (result < min || result > max) ? 0 : result
+    return (reverseSting > max) ? 0 : isNegative ? -1*reverseSting :  reverseSting;
 };
