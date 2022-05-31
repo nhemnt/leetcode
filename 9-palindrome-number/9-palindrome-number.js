afcae151fa32398096a5c6ed2bf467a6d2e83850
Hemant Negi <nhemnt@gmail.com>
2022-05-31 23:14:25 +0530

    Time: 279 ms (34.85%), Space: 51.1 MB (51.75%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(x < 0 ) return false;
+    if(x < 0 || (x % 10 === 0 && x !== 0) ) return false;
     if(x  < 10 ) return true;
     // console.log({x , str: x.toString().split("").reverse().join("")});
     return x.toString() == x.toString().split("").reverse().join("");
