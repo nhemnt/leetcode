1ec6aeea931327f5129dc46999d5c3a58c3a5b23
Hemant Negi <nhemnt@gmail.com>
2022-05-31 18:59:32 +0530

    Time: 85 ms (77.48%), Space: 44.3 MB (72.55%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.js
@@ -4,20 +4,24 @@
  */
 var myAtoi = function(str) {
     
-    const s = str.trim();
-    if(!(!isNaN(s[0]) || s[0] === "+" || s[0] === "-")) return 0;
-    let result = !isNaN(s[0]) ? s[0]: '';
-    let isNegative = s[0] === "-";
-     for(let i=1; i<s.length; i++){
-         console.log({s: s[i], length: s[i].length});
-        if(!isNaN(s[i]) && s[i] !== " ")  {
-            result += s[i]
-        }else{
-            break;
-        };
-    };
-    if(result === "") return 0;
-    const max = Math.pow(2,31) -1;
-    const output = Math.min(parseInt(result),isNegative ?  max +1 : max);
-    return isNegative ? -1*output : output;
+    // const s = str.trim();
+    // if(!(!isNaN(s[0]) || s[0] === "+" || s[0] === "-")) return 0;
+    // let result = !isNaN(s[0]) ? s[0]: '';
+    // let isNegative = s[0] === "-";
+    //  for(let i=1; i<s.length; i++){
+    //      // console.log({s: s[i], length: s[i].length});
+    //     if(!isNaN(s[i]) && s[i] !== " ")  {
+    //         result += s[i]
+    //     }else{
+    //         break;
+    //     };
+    // };
+    //  const max = Math.pow(2,31) -1;
+    // const output = Math.min(parseInt(result),isNegative ?  max +1 : max);
+    // return isNegative ? -1*output : output;
+    const output = parseInt(str);
+    if(isNaN(output)) return 0;
+    const max = Math.pow(2,31)
+    return Math.max(max* -1, Math.min(max-1, output));
+   
 };
