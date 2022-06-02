5d2e083c7d690950ed2eb51974b80219ad8b89da
Hemant Negi <nhemnt@gmail.com>
2022-06-02 13:58:11 +0530

    Time: 94 ms (49.69%), Space: 44.8 MB (47.19%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.js
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -4,12 +4,12 @@
  */
 var transpose = function(matrix) {
     let newArr = []
-    for(let i=0;i<matrix.length; i++){
-        for(let j=0; j<matrix[i].length; j++){
-            if(!newArr[j]) newArr.push([])
-            newArr[j].push(matrix[i][j])
+    for(let i=0;i<matrix[0].length; i++){
+        const col = []
+        for(let j=0; j<matrix.length; j++){
+            col.push(matrix[j][i])
         }
-        
+        newArr.push(col);
     }
     // console.log(newArr);
     return newArr;
