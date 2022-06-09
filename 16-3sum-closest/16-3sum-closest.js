59dd789c038c127167ace80e56c483c9ab74357f
Hemant Negi <nhemnt@gmail.com>
2022-06-09 23:35:26 +0530

    Time: 127 ms (26.85%), Space: 42.7 MB (58.74%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.js
+++ 16-3sum-closest/16-3sum-closest.js
@@ -7,15 +7,14 @@ var threeSumClosest = function(nums, target) {
    
     if(nums.length <4 ) return nums.reduce((partialSum, a) => partialSum + a, 0);
     nums.sort((a,b) => a-b );
-    let nearest;
+    let nearest = Infinity;
     for(let i =0 ; i< nums.length; i++){
          let j=i+1;
          let k = nums.length-1;
         while(j<k){
             const sum = nums[i] + nums[j] + nums[k];
-            if(nearest === undefined) nearest = sum;
 
-            else if(sum > target) k--;
+            if(sum > target) k--;
             else j++;
             
             const diff = Math.abs(target - sum);
