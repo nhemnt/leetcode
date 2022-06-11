04ba9c912eb5fff42821e4bb07a59eb4f931ca4e
Hemant Negi <nhemnt@gmail.com>
2022-06-12 00:25:14 +0530

    Time: 252 ms (30.00%), Space: 89.8 MB (10.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
@@ -18,26 +18,37 @@
 //     return result === Infinity ?  -1 : result;
 // };
 
+
+// [[A] , [B], [C] ]
+// [A] + [C] = x
+// x - [A] = [C];
+// [1 ,1 ,4,2,3]  => input
+// [0 ,1 ,2,3,4]  => index
+// [11,10,9,5,3]  => rSum
+// 5 => target
+   // [3] => i =1, j = 4 (nums.length - j)
+
+
 var minOperations = function(nums, x) {
-  let result = Infinity
-      
-      
-  const sumRight = new Map([[0, nums.length]])
-  let temp = 0
-  for (let i = nums.length - 1; i >= 0; i--) {
-    temp += nums[i]
-    sumRight.set(temp, i)
+  let result = Infinity;
+  let rSumMap = new Map([[0, nums.length]]);
+  let temp = 0;
+  for(let i = nums.length-1; i>=0; i--){
+    temp += nums[i];
+    rSumMap.set(temp,i);
   }
-  temp = 0
-  for (let i = -1; i < nums.length; i++) {
-    temp += i < 0 ? 0 : nums[i]
-    if (temp > x) break
-    const possibleIndex = sumRight.get(x - temp)
-    if (possibleIndex !== undefined && possibleIndex > i) {
-      const steps = i + 1 + (nums.length - 1 - possibleIndex + 1)
-      result = Math.min(result, steps)
-    }
-  }
+  // console.log(rSumMap);
   
-  return result === Infinity ? -1 : result
+  temp = 0;
+  for(let i =-1; i<nums.length; i++){
+    temp += i < 0 ? 0 : nums[i];
+    if(temp > x) break;
+    const j = rSumMap.get(x-temp);
+    if(j !== undefined && j > i){
+      const steps = i + 1 + nums.length - j;
+      result = Math.min(steps, result);
+    }
+    
+  }
+  return result === Infinity ? -1 : result;
 };
