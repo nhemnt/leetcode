d4afee50dfdf920125e3047908f009ad5bcd9671
Hemant Negi <nhemnt@gmail.com>
2022-06-15 22:37:12 +0530

    Time: 292 ms (24.83%), Space: 64.3 MB (9.01%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var longestStrChain = function(words) {
+  words.sort((x,y) => x.length - y.length );
+  const map = {};
+  let max = 0;
+  for(let word of words){
+    let l =0
+    for(let i= 0; i< word.length; i++){
+      const sub = word.substr(0, i) + word.substr(i+1, word.length);
+      // console.log({sub, word, i})
+      l = Math.max(l, (map[sub] || 0) +1 );      
+    }
+    map[word] = l;
+    max = Math.max(max, l);
+  }
+  
+  return max;
+};
