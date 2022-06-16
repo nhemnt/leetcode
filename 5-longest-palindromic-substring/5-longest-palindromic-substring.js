91c0e7b5b35df320dab28d49bbfc7cf7fa3b1b40
Hemant Negi <nhemnt@gmail.com>
2022-06-16 12:58:05 +0530

    Time: 1772 ms (11.97%), Space: 89.3 MB (5.18%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -4,31 +4,54 @@
  */
 var longestPalindrome = function(s) {
     if(s.length === 1) return s;
-    let max = 0, m, n, result = '';
-    for(let i =0; i<s.length; i++){
-        for(let j=i+1; j<s.length; j++){
-            let flag = 1;
-            for(k=0; k< (j - i + 1)/2 ; k++){
-                // console.log({si: s[i+k], sj : s[j-k]}, flag)
-                if(s[i+k] !== s[j-k]){
-                    flag = 0;
-                    break;
+    let max = 0;
+
+    const dp = [...new Array(s.length)].map(() => new Array(s.length).fill(''))
+
+    let i =0, j= 0; k =0;
+    let m,n;
+    while(i<s.length){
+        if(i === 0){
+            while(j<s.length){
+                // console.log({j,k});
+                dp[j][k] = 1;
+                j++;
+                k++
+                if(k-j+1 > max){
+                    max = k-j+1;
+                    m = j;
+                    n = k;
                 }
             }
-            // console.log(flag)
-            if(flag !== 0 && (j-i+1) > max){
-                // console.log({j,i,max});
-                max = j-i+1;
-                m = i;
-                n = j;
+        }else{
+            j = 0; k =i;
+            
+            while(k<s.length){
+                // console.log({i,j ,k ,l: s[i], r: s[k]});
+                if(s[j] === s[k] && k-j ===1 ){
+                    dp[j][k] = 1
+                    if(k-j+1 > max){
+                        max = k-j+1;
+                        m = j;
+                        n = k;
+                    }
+                }else if(s[j] === s[k] && dp[j+1][k-1]){
+                    dp[j][k] = 1
+                    if(k-j+1 > max){
+                        max = k-j+1;
+                        m = j;
+                        n = k;
+                    }
+                }else {
+                    dp[j][k] = 0;
+                }
+                j++; k++
             }
+
         }
+        i++;
     }
-    // console.log({m,n});
-    for(let i = m; i<=n; i++){
-        result += s[i];
-    }
-    
-    return result || s[0];
-    
+    // console.log(dp);
+    // console.log({m, n})
+    return s.substring(m, n+1);
 };
