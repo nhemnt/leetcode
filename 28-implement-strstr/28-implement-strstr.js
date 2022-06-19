c1be69b43e3a114ca68cc3527d232ce24a07d341
Hemant Negi <nhemnt@gmail.com>
2022-06-19 17:39:18 +0530

    Time: 76 ms (63.90%), Space: 41.6 MB (93.75%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -3,6 +3,20 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-  return haystack.indexOf(needle)
+var strStr = function(haystack, needle) {  
+  let i =0, j=0;
+
+  while(j<haystack.length){
+    if(j-i + 1  !== needle.length){
+      j++;
+    }else {
+      if(haystack.substr(i,needle.length) === needle){
+        return i;
+      }
+      i++;
+    }
+    
+  }
+
+  return -1
 };
