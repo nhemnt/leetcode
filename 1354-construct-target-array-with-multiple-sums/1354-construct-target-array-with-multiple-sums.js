2454af64855811104edb213065bef158eb369012
Hemant Negi <nhemnt@gmail.com>
2022-06-24 17:15:08 +0530

    Time: 243 ms (16.67%), Space: 56.8 MB (16.67%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var isPossible = function(t) {
+  const q = new MaxPriorityQueue({priority: x => x});
+  let sum =0;
+  for(let i =0; i<t.length; i++){
+    sum +=t[i];
+    q.enqueue(t[i]);
+  }
+  
+  while(q.front().element !== 1){
+    let f = q.dequeue().element;
+    sum -= f;
+    if(f <= sum || sum < 1) return false;
+    console.log({sum, f})
+    f %= sum;
+    sum += f;
+    q.enqueue(f || sum)
+  }
+  return true;
+};
