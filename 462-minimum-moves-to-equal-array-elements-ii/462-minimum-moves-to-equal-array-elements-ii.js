b3231cc290285c7bd1556805f214fda6a6fae1cb
Hemant Negi <nhemnt@gmail.com>
2022-06-30 23:09:09 +0530

    Time: 117 ms (25.00%), Space: 44.4 MB (22.50%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.js
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.js
@@ -3,15 +3,23 @@
  * @return {number}
  */
 var minMoves2 = function(n) {
-  // n2
-  let min = Infinity;
-  for(let i =0; i<n.length; i++){
-    let sum = 0
-    for(let j =0; j<n.length; j++){
-      sum += Math.abs(n[i] - n[j]);
-    }
-    min = Math.min(min, sum)
-  }
+// n2
+//   let min = Infinity;
+//   for(let i =0; i<n.length; i++){
+//     let sum = 0
+//     for(let j =0; j<n.length; j++){
+//       sum += Math.abs(n[i] - n[j]);
+//     }
+//     min = Math.min(min, sum)
+//   }
+// return min
+
+  n.sort((x,y) => x-y);
   
-  return min
+  const m = Math.floor(n.length/2)
+  let ans = 0
+  for(const k of n){
+    ans += Math.abs(n[m]-k)
+  }
+  return ans;
 };
