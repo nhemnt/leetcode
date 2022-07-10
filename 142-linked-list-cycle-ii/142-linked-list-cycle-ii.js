f67de24abdb9ab09efaa464ef60cfb9a4c8d6304
Hemant Negi <nhemnt@gmail.com>
2022-07-10 22:53:16 +0530

    Time: 153 ms (10.54%), Space: 45.6 MB (14.19%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+  if(!head) return head;
+  let p1 = head;
+  let p2 = head;
+  let p1c =0, p2c = 0;
+  while(p2 && p2.next && p2.next){
+    p1 = p1.next;
+    p2 = p2.next.next;
+    if(p1 === p2){
+      p1 = head;
+      while(p1 !== p2){
+        p1 = p1.next;
+        p2 = p2.next;
+      }
+      return p1;
+    }
+  }
+  return null;
+};
