622e6d95c98e8bda1aaa1a301b98dc49fb29df77
Hemant Negi <nhemnt@gmail.com>
2022-07-12 21:55:18 +0530

    Time: 121 ms (54.19%), Space: 51.7 MB (67.94%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  var min = Infinity;
-  var max = 0
+  let min = Infinity;
+  let max = 0
   for(let i =0; i<prices.length; i++){
     min = Math.min(min, prices[i]);
     max = Math.max(max, prices[i] -min);
