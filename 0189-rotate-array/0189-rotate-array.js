9601e390d7b45b8f6097a4371708e0d36fe94bcd
Hemant Negi <nhemnt@gmail.com>
2024-06-29 14:22:44 +0530

    Time: 92 ms (20.27%), Space: 59.9 MB (32.33%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -3,21 +3,23 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-    if(k === nums.length) return nums;
-    if(k > nums.length) {
-        k = k % nums.length
+var rotate = function (nums, k) {
+    if (k === nums.length) return nums;
+
+    if (k > nums.length) {
+        k = k % nums.length;
     }
-    const temp = []
-    for(let i=nums.length-k; i<nums.length; i++){
+
+    const temp = [];
+    for (let i = nums.length - k; i < nums.length; i++) {
         temp.push(nums[i]);
     }
 
-    for(let i=nums.length-k-1; i>=0; i--){
-        nums[i+k] = nums[i];
+    for (let i = nums.length - k - 1; i >= 0; i--) {
+        nums[i + k] = nums[i];
     }
 
-    for(let i= 0; i< k; i++){
+    for (let i = 0; i < k; i++) {
         nums[i] = temp[i];
     }
 
