4145c4ad53fac63fe1787cf6f0ff5eadfbd67ffa
Hemant Negi <nhemnt@gmail.com>
2024-06-29 14:01:32 +0530

    Time: 64 ms (44.06%), Space: 51.1 MB (72.5%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,19 +3,15 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let res = 0, count =1;
-    for(let i =1; i< nums.length; i++){
-        if(nums[res] === nums[i]){
-            count++
-        }else{
-            count--
-        }
+    let result = nums[0], count = 1;
+    for(let i =1 ; i< nums.length; i++){
+        count = nums[i] === result ? ++count : --count;
 
         if(count === 0){
-            res= i;
-            count =1;
+            result = nums[i];
+            count = 1;
         }
     }
 
-    return nums[res];
+    return result;
 };
