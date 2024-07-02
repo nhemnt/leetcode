d7ae3b4f249da44740ff2fe20610d2b911d87fc0
Hemant Negi <nhemnt@gmail.com>
2024-07-03 00:27:04 +0530

    Time: 50 ms (82.87%), Space: 51.4 MB (16.62%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -2,6 +2,35 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-    return s.trim().split(" ").filter(x => x ).reverse().join(" ");
+var reverseWords = function (s) {
+    // return s.trim().split(" ").filter(x => x ).reverse().join(" ");
+    const result = [];
+    let str = "";
+
+    for (let i = 0; i < s.length; i++) {
+        if (str) {
+            if (s[i] === " ") {
+                result.push(str)
+                str = ""
+            } else {
+                str += s[i];
+            }
+
+        } else if (s[i] !== " ") {
+            str = s[i];
+        }
+
+        if (i === s.length - 1 && str) {
+            result.push(str)
+        }
+
+    }
+    let i = 0; j = result.length - 1;
+    while (i < j) {
+        const temp = result[i];
+        result[i] = result[j];
+        result[j] = temp;
+        i++; j--;
+    }
+    return result.join(" ");
 };
