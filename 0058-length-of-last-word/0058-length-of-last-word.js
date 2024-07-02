db3696cc220e4069bd2307f06de5fa2bf0ff758f
Hemant Negi <nhemnt@gmail.com>
2024-07-02 22:54:29 +0530

    Time: 69 ms (6.12%), Space: 48.9 MB (48.44%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,21 +3,21 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    return s.trim().split(" ").pop().length;
-    // let count = 0, start = false;
+    // return s.trim().split(" ").pop().length;
+    let count = 0, start = false;
 
-    // for(let i = s.length-1; i >=0; i--){
-    //     if(start){
-    //         if(s[i] === " "){
-    //             return count
-    //         }
-    //         count++;
-    //         continue;
-    //     }else if(s[i] !== " "){
-    //         start = true;
-    //         count++;
-    //     }
-    // }
+    for(let i = s.length-1; i >=0; i--){
+        if(start){
+            if(s[i] === " "){
+                return count
+            }
+            count++;
+            continue;
+        }else if(s[i] !== " "){
+            start = true;
+            count++;
+        }
+    }
 
-    // return count;
+    return count;
 };
