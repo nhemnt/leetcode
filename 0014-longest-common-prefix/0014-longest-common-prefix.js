6f3d6ae60e2759a9bac4e70775de4951a6f63b76
Hemant Negi <nhemnt@gmail.com>
2024-07-03 00:11:29 +0530

    Time: 68 ms (12.3%), Space: 51.6 MB (12.57%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    if (strs.length === 0) return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        let j = 0;
+        while (j < prefix.length && j < strs[i].length && prefix[j] === strs[i][j]) {
+            j++;
+        }
+        prefix = prefix.substring(0, j);
+        if (prefix === "") return "";
+    }
+
+    return prefix;
+};
