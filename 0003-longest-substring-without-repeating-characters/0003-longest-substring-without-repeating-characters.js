906ec95f66e56a75bab89b9d767b1658037799d8
Hemant Negi <nhemnt@gmail.com>
2024-07-07 00:17:10 +0530

    Time: 510 ms (5.03%), Space: 61.4 MB (5.25%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let obj = {};
+    let count = 1;
+    let max = s.length === 1 ? 1: 0
+    obj[s[0]] = 0;
+    for(let i=1; i<s.length; i++){
+        // console.log(i);
+        if(obj[s[i]] !== undefined){
+            count = 0;
+            i= obj[s[i]];
+            obj = {}
+        }else{
+            // console.log({key: s[i], count})
+            obj[s[i]] = i;
+            count++
+            max = Math.max(count,max);
+        }
+    }
+    return max;
+};
