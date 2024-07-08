5119746f02bf19c95f3bb6c0e09a3f34121219e3
Hemant Negi <nhemnt@gmail.com>
2024-07-08 15:11:19 +0530

    Time: 89 ms (12.52%), Space: 52.8 MB (47.85%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -3,30 +3,29 @@
  * @param {string} magazine
  * @return {boolean}
  */
-var canConstruct = function(ransomNote, magazine) {
+var canConstruct = function (ransomNote, magazine) {
     const ransomNoteLetterMap = {}
     const magazineLetterMap = {}
-    for(let i = 0; i< magazine.length; i++){
-        if(magazineLetterMap[magazine[i]]){
-            magazineLetterMap[magazine[i]]++
+    for (let i = 0; i < ransomNote.length; i++) {
+        if (ransomNoteLetterMap[ransomNote[i]]) {
+            ransomNoteLetterMap[ransomNote[i]] +=  1
         }else{
-            magazineLetterMap[magazine[i]] = 1
+            ransomNoteLetterMap[ransomNote[i]] = 1;
         }
     }
-    for(let i = 0; i< ransomNote.length; i++){
-        if(ransomNoteLetterMap[ransomNote[i]]){
-            ransomNoteLetterMap[ransomNote[i]]++
+     for (let i = 0; i < magazine.length; i++) {
+        if (magazineLetterMap[magazine[i]]) {
+            magazineLetterMap[magazine[i]] +=  1
         }else{
-            ransomNoteLetterMap[ransomNote[i]] = 1
+            magazineLetterMap[magazine[i]] = 1;
         }
     }
 
-    for(key in ransomNoteLetterMap){
-        if(ransomNoteLetterMap[key] > (magazineLetterMap[key] || 0) ){
+    for(let key in ransomNoteLetterMap){
+        if(!magazineLetterMap[key] || magazineLetterMap[key] < ransomNoteLetterMap[key]){
             return false;
         }
     }
-
     return true;
-    
+
 };
