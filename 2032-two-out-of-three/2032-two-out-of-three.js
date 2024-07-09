556702b7731781d1302306ed04296b3425d1bd80
Hemant Negi <nhemnt@gmail.com>
2024-07-09 19:47:21 +0530

    Time: 71 ms (42.4%), Space: 51.2 MB (97.15%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.js
+++ 2032-two-out-of-three/2032-two-out-of-three.js
@@ -5,54 +5,22 @@
  * @return {number[]}
  */
 var twoOutOfThree = function (nums1, nums2, nums3) {
-    const map = {};
-    // nums1 = new Set(nums1)
-    // nums2 = new Set(nums2)
-    // nums3 = new Set(nums3)
-    let i = 0;
-    const updateMap = (key) => {
-        if (map[key]) {
-            map[key]++;
-        } else {
-            map[key] = 1
-        }
-    }
-    while (true) {
-        if (i >= nums1.length && i >= nums2.length && i >= nums3.length) {
-            break;
-        }
-        if (i < nums1.length) {
-            updateMap(`1__${nums1[i]}`)
-        }
-        if (i < nums2.length) {
-            updateMap(`2__${nums2[i]}`)
-        }
-        if (i < nums3.length) {
-            updateMap(`3__${nums3[i]}`)
-        }
-        i++
+    let countMap = {};
+    
+    function countOccurrences(nums) {
+        let uniqueNums = new Set(nums);
+        uniqueNums.forEach(num => {
+            if (countMap[num]) {
+                countMap[num]++;
+            } else {
+                countMap[num] = 1;
+            }
+        });
     }
 
+    countOccurrences(nums1);
+    countOccurrences(nums2);
+    countOccurrences(nums3);
 
-    const nMap = {}
-    const updatenMap = (key) => {
-        if (nMap[key]) {
-            nMap[key]++;
-        } else {
-            nMap[key] = 1
-        }
-    }
-    for(let key in map){
-        const k = key.split("__")[1];
-        updatenMap(k);
-    }
-    const result = []; 
-
-    for(let key in nMap){
-        if(nMap[key] > 1){
-            result.push(key);
-        }
-    }
-
-    return result;
+    return Object.keys(countMap).filter(key => countMap[key] > 1).map(Number);
 };
