8cad83bd5c961b2c227c60d4e4f4ae1ac9373056
Hemant Negi <nhemnt@gmail.com>
2024-07-10 11:31:52 +0530

    Time: 66 ms (52.48%), Space: 52.2 MB (95.36%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -10,19 +10,20 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
+var hasCycle = function (head) {
     let p1 = head;
     let p2 = head;
 
-    while(p1 && p2){
+    while (p1 && p2) {
         p1 = p1.next;
         p2 = p2.next;
 
-        if(!p2) return false;
-        
+        if (!p2) return false
+
         p2 = p2.next;
 
-        if(p1 === p2) return true;
+        if (p1 === p2) return true;
     }
-    return false
+
+    return false;
 };
