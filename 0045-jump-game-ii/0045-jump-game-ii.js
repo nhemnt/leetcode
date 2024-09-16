a720b79fbd4e9dc05f46a2bb27b61b16ea99e3c6
Hemant Negi <nhemnt@gmail.com>
2024-09-16 23:35:48 +0530

    Time: 724 ms (5.06%), Space: 52.5 MB (10.42%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -2,24 +2,44 @@
  * @param {number[]} nums
  * @return {number}
  */
-var jump = function(nums) {
-    let max = 0;
-    let oldMax = 0;
-    let jump =0;
+var jump = function(nums, length = 0, memo= {}) {
+    // let max = 0;
+    // let oldMax = 0;
+    // let jump =0;
 
-    for(let i =0; i<nums.length; i++){
-        if(nums.length < 2) return 0
-        max = Math.max(max, i + nums[i]);
+    // for(let i =0; i<nums.length; i++){
+    //     if(nums.length < 2) return 0
+    //     max = Math.max(max, i + nums[i]);
 
-        if(i === oldMax){
-            oldMax = max;
-            jump++;
+    //     if(i === oldMax){
+    //         oldMax = max;
+    //         jump++;
 
-            if(oldMax >= nums.length -1){
-                break
-            }
+    //         if(oldMax >= nums.length -1){
+    //             break
+    //         }
+    //     }
+    // }
+
+    // return jump;
+
+    if(length >= nums.length-1) return 0;
+
+    if(memo[length] !== undefined) return memo[length];
+
+    const maxJump = nums[length];
+
+    if(maxJump === 0) return Infinity;
+
+    let minJump = Infinity;
+
+    for(let i =1; i<= maxJump; i++){
+        const nextJump = jump(nums, i + length, memo);
+        if(nextJump !== Infinity){
+            minJump = Math.min(minJump, nextJump +1);
         }
     }
+    memo[length]= minJump;
+    return minJump;
 
-    return jump;
 };
